198ae88e7306c80fd93f4139d59f04241902a5ce
Dennis Dunn <ansofive@gmail.com>
2016-06-29 21:30:52 -0400

    No more squiggles, must be OK.

---
+++ src/codesets.ts
@@ -0,0 +1,6 @@
+export {default as antigenSeries} from "./data/antigen-series";
+export {default as contraindications }from "./data/antigen-series";
+export {default as cvxToAntigenMap }from "./data/cvx-to-antigen-map";
+export {default as liveVirusConflicts} from "./data/live-virus-conflicts";
+export {default as vaccineGroup }from "./data/vaccine-group";
+export {default as vaccineGroupToAntigenMap }from "./data/vaccine-group-to-antigen-map";
--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,25 @@
-﻿import items from "./data/antigen-series";
+﻿import * as codesets from "./codesets"
 
+export class LookupTable {
+    private items: any[];
+    private callback: (item: any) => string;
+    public constructor(items: any[], getKeyValue: (item: any) => string) {
+        this.items = items;
+        this.callback = getKeyValue;
+    }
+    public get(key: string) {
+        return this.items.filter((item: any) => {
+            return this.callback(item) === key;
+        })
+    }
+}
 
-/**
- * A table consists of an array of data and functions to access that data.
- * The keyFunc selects the data out of an item that is used to identify that
- * item, it's signature is f(item). findfunc is a function used to select
- * an item from the array and it's signature is f(item, key), the item
- * is selected if f() == true
-*/
-// function table(data, keyFunc, findFunc) {
-//     return {   
-//         "keys" : function(){
-//             var items = data.map(keyFunc); 
-//             return items.filter(function(val, i, arr){
-//                 return (i <= arr.indexOf(val));
-//             });
-//         },
-//         "find" : function(key){
-//             if(key == undefined) {
-//                 return data;
-//             }
-//             else {
-//                 var predicate = function(i){ // currying!
-//                     return findFunc(i, key);
-//                 };
-//                 var items = data.filter(predicate);
-//                 if (items.length==0)
-//                     return undefined;
-//                 else if(items.length==1)
-//                     return items[0];
-//                 else
-//                     return items;
-//             }
-//         }
-//     };
-// };
+export class RefData {
+    static Antigens = new LookupTable(codesets.antigenSeries, (item: any) => item.Name);
+    static Contraindications = new LookupTable(codesets.contraindications, (item: any) => item.Antigen);
+    static CvxToAntigens = new LookupTable(codesets.cvxToAntigenMap, (item: any) => item.CvxCode);
+    static LiveVirusConflicts = new LookupTable(codesets.liveVirusConflicts, (item: any) => item.CurrentVaccineType.Name);
+    static VaccineGroup = new LookupTable(codesets.vaccineGroup, (item: any) => item.Name);
+    static VaccineGroupToAntigens = new LookupTable(codesets.vaccineGroupToAntigenMap, (item: any) => item.VaccineGroup);
+}
 
-// module.exports = {
-//     'AntigenSeries': new table(require('./data/antigen-series'), function(i) {return i.Name;}, function(i,k){return i.Name == k}),
-//     'Contraindications': new table(require('./data/contraindications'),function(i) {return i.Antigen;}, function(i,k){return i.Antigen == k}),
-//     'Cvx2Antigen': new table(require('./data/cvx-to-antigen-map'),function(i) {return i.CvxCode;}, function(i,k){return i.CvxCode == k}),
-//     'LiveVirusConflicts': new table(require('./data/live-virus-conflicts'),function(i){return i.PreviousVaccineType.Name;}, function(i,k){return i.PreviousVaccineType.Name == k}),
-//     'Testcases': new table(require('./data/test-cases'),function(i){return i.CdcTestId;}, function(i,k){return i.CdcTestId == k}),
-//     'VaccineGroup2Antigen': new table(require('./data/vaccine-group-to-antigen-map'), function(i) {return i.VaccineGroup;}, function(i,k){return i.VaccineGroup == k}),
-//     'VaccineGroups': new table(require('./data/vaccine-group'), function(i) {return i.Name;}, function(i,k){return i.Name == k})
-// };
