3bba0545984f103740b8fe44be5d3db59db041c9
Dennis Dunn <ansofive@gmail.com>
2016-06-29 21:48:13 -0400

    Compile OK.

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+﻿import * as codesets from "./codesets"
+
+export class LookupTable {
+    private items: any[];
+    private callback: (item: any) => string;
+    public constructor(items: any[], getKeyValue: (item: any) => string) {
+        this.items = items;
+        this.callback = getKeyValue;
+    }
+    public get(key: string) {
+        return this.items.filter((item: any) => {
+            return this.callback(item) === key;
+        })
+    }
+}
+
+export class RefData {
+    static Antigens = new LookupTable(codesets.antigenSeries, (item: any) => item.Name);
+    static Contraindications = new LookupTable(codesets.contraindications, (item: any) => item.Antigen);
+    static CvxToAntigens = new LookupTable(codesets.cvxToAntigenMap, (item: any) => item.CvxCode);
+    static LiveVirusConflicts = new LookupTable(codesets.liveVirusConflicts, (item: any) => item.CurrentVaccineType.Name);
+    static VaccineGroup = new LookupTable(codesets.vaccineGroup, (item: any) => item.Name);
+    static VaccineGroupToAntigens = new LookupTable(codesets.vaccineGroupToAntigenMap, (item: any) => item.VaccineGroup);
+}
+
