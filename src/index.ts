3f7efc2e6f04befe551934959e0f46456e908d0e
Dennis Dunn <ansofive@gmail.com>
2016-06-29 22:12:17 -0400

    Change the RefData class to a module.

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,12 @@ export class LookupTable {
     }
 }
 
-export class RefData {
-    static Antigens = new LookupTable(codesets.antigenSeries, (item: any) => item.Name);
-    static Contraindications = new LookupTable(codesets.contraindications, (item: any) => item.Antigen);
-    static CvxToAntigens = new LookupTable(codesets.cvxToAntigenMap, (item: any) => item.CvxCode);
-    static LiveVirusConflicts = new LookupTable(codesets.liveVirusConflicts, (item: any) => item.CurrentVaccineType.Name);
-    static VaccineGroup = new LookupTable(codesets.vaccineGroup, (item: any) => item.Name);
-    static VaccineGroupToAntigens = new LookupTable(codesets.vaccineGroupToAntigenMap, (item: any) => item.VaccineGroup);
+module RefData {
+    var Antigens = new LookupTable(codesets.antigenSeries, (item: any) => item.Name);
+    var Contraindications = new LookupTable(codesets.contraindications, (item: any) => item.Antigen);
+    var CvxToAntigens = new LookupTable(codesets.cvxToAntigenMap, (item: any) => item.CvxCode);
+    var LiveVirusConflicts = new LookupTable(codesets.liveVirusConflicts, (item: any) => item.CurrentVaccineType.Name);
+    var VaccineGroup = new LookupTable(codesets.vaccineGroup, (item: any) => item.Name);
+    var VaccineGroupToAntigens = new LookupTable(codesets.vaccineGroupToAntigenMap, (item: any) => item.VaccineGroup);
 }
 
