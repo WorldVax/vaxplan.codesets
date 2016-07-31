a15c2c6ece2bfc17557b31781485ddaef8d7d28a
Dennis Dunn <ansofive@gmail.com>
2016-07-30 23:06:53 -0400

    Add a working transform from xml to json.

--- src/generate.js
+++ src/generate.js
@@ -1,1 +1,18 @@
 // Transform the xml files to json.
+var fs = require('fs'),
+    xml = require('xml2js'),
+    path = require('path'),
+    glob = require('glob');
+
+glob(__dirname + '\\data\\xml\\*.xml', function (err, files) {
+    files.forEach(function (file) {
+        var basename = path.basename(file, '.xml');
+        console.log(basename);
+        fs.readFile(file, 'utf8', function (err, data) {
+            var parser = new xml.Parser();
+            parser.parseString(data, function (err, result) {
+                fs.writeFile(__dirname + '\\data\\json\\' + basename + '.json', JSON.stringify(result,'',4));
+            });
+        });
+    });
+});
