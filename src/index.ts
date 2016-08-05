6477ae3e5101fe58dbdda62126d9a05339fad753
Dennis Dunn <ansofive@gmail.com>
2016-08-04 22:32:03 -0400

    Add the antigen series to the module.

--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,22 @@ var antigens:any = require("./AntigenSeries");
 
 export class LookupTable {
     private items: any[];
-    private selectorCallback: (item: any) => string;
-    public constructor(items: any[], selector: (key: string) => any, keys: () => string[]) {
+    private selectorCallback: (table: any, key: string) => any;
+    private keysCallback: (table: any) => string[];
+    public constructor(items: any[], selector: (table: any, key: string) => any, keys: (table: any) => string[]) {
         this.items = items;
         this.selectorCallback = selector;
         this.keysCallback = keys;
     }
     public get(key: string) {
-        return this.selectorCallback.call(this.items, key);
+        return this.selectorCallback(this.items, key);
     }
     public keys() {
-        return this.keysCallback.call(this.items);
+        return this.keysCallback(this.items);
     }
 }
 
 module RefData {
-    var Antigens = new LookupTable(antigens, (key: string) => this[key], () => Object.keys(this);
-    var Contraindications = new LookupTable(codesets.ScheduleSupportingData.contraindications, (key: string) => item.Antigen);
-    var CvxToAntigens = new LookupTable(codesets.cvxToAntigenMap, (item: any) => item.CvxCode);
-    var LiveVirusConflicts = new LookupTable(codesets.liveVirusConflicts, (item: any) => item.CurrentVaccineType.Name);
-    var VaccineGroup = new LookupTable(codesets.vaccineGroup, (item: any) => item.Name);
-    var VaccineGroupToAntigens = new LookupTable(codesets.vaccineGroupToAntigenMap, (item: any) => item.VaccineGroup);
+    var Antigens = new LookupTable(antigens, (table: any, key: string) => table[key], (table) => Object.keys(table));
 }
 
