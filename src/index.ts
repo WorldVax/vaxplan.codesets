9e633970da228702c2978ba7247935434a1ed7f2
Dennis Dunn <ansofive@gmail.com>
2016-08-17 12:55:48 -0400

    Use the simplified lookup-table-factory method.

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,27 @@
 
 let codesets:any = require("./ScheduleSupportingData");
 let antigens:any = require("./AntigenSeries");
- 
- export interface IRefData {
-     get(key:any) : any;
-     keys() : any[];
- }
 
-export class ObjectLookupTable implements IRefData {
-    private items: any[];
-    public get(key : any) { return this.items[key]; }
-    public keys() { return Object.keys(this); }
-    public constructor(items: any[]) {
-        this.items = items;
-    }
+export interface IRefData {
+    get(key:any) : any;
+    keys() : any[];
 }
 
-export class ArrayLookupTable implements IRefData {
-    private key: string;
-    private items: any[];
-    public get(key : any) { return this.items.filter(function(x) {return x[this.key] == key;}) }
-    public keys() {return this.items.map(function(x) {return x[this.key];}) }
-    public constructor(items: any[], key: string) {
-        this.items = items;
-        this.key = key;
+function make(data:any, getName: (item:any) => string) : any
+{
+    let lut = Object.create(data);
+    if(data instanceof Array){
+        data.forEach((item) => {
+            let name = getName(item);
+            Object.defineProperty(lut, name, {"value":item, "enumerable":true});
+        });
     }
-}
+    return lut;
+} 
 
-export let Antigens = new ObjectLookupTable(antigens);
-export let LiveVirusConflicts = new ArrayLookupTable(codesets.scheduleSupportingData.liveVirusConflicts[0].liveVirusConflict, "name");
-export let Contraindications = new ArrayLookupTable(codesets.scheduleSupportingData.contraindications[0].contraindication, "antigen");
-export let VaccineGroups = new ArrayLookupTable(codesets.scheduleSupportingData.vaccineGroups[0].vaccineGroup, "name");
-export let VaccineGroupToAntigenMap = new ArrayLookupTable(codesets.scheduleSupportingData.vaccineGroupToAntigenMap[0].vaccineGroupMap, "name");
-export let CvxToAntigenMap = new ArrayLookupTable(codesets.scheduleSupportingData.cvxToAntigenMap[0].cvxMap, "cvx");
+export let Antigens = make(antigens, null);
+//export let LiveVirusConflicts = make(codesets.scheduleSupportingData.liveVirusConflicts[0].liveVirusConflict, (item) => {return item["current"][0]["vaccineType"];});
+//export let Contraindications = new ArrayLookupTable(codesets.scheduleSupportingData.contraindications[0].contraindication, "antigen");
+export let VaccineGroups = make(codesets.scheduleSupportingData.vaccineGroups[0].vaccineGroup, (item) => {return item["name"];});
+export let VaccineGroupToAntigenMap = make(codesets.scheduleSupportingData.vaccineGroupToAntigenMap[0].vaccineGroupMap, (item) => {return item["name"];});
+export let CvxToAntigenMap = make(codesets.scheduleSupportingData.cvxToAntigenMap[0].cvxMap, (item) =>{ return "_" + item["cvx"];});
