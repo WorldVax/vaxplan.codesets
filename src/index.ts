f8a6b17015110ca429a3d285eabf376ec4717a98
Dennis Dunn <ansofive@gmail.com>
2016-08-17 13:53:18 -0400

    Add Contraindications to the reference data.

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,21 @@ function make(data:any, getName: (item:any) => string) : any
     return lut;
 } 
 
+function makelists(data:any, name :string) : any
+{
+    let lut = Object.create(data);
+    let keys = data.map((item)=>{return item[name];})
+    keys = keys.filter((val, i, arr) => {return (i <= arr.indexOf(val));});
+    keys.forEach((key) =>{
+        let items = data.filter((item)=> {return item[name] == key;});
+        Object.defineProperty(lut, key, {"value":items, "enumerable":true});
+    });
+    return lut;
+}
+
 export let Antigens = make(antigens, null);
-//export let LiveVirusConflicts = make(codesets.scheduleSupportingData.liveVirusConflicts[0].liveVirusConflict, (item) => {return item["current"][0]["vaccineType"];});
-//export let Contraindications = new ArrayLookupTable(codesets.scheduleSupportingData.contraindications[0].contraindication, "antigen");
+//export let LiveVirusConflicts = makelists(codesets.scheduleSupportingData.liveVirusConflicts[0].liveVirusConflict, (item) => {return item["current"][0]["vaccineType"];});
+export let Contraindications = makelists(codesets.scheduleSupportingData.contraindications[0].contraindication, "antigen");
 export let VaccineGroups = make(codesets.scheduleSupportingData.vaccineGroups[0].vaccineGroup, (item) => {return item["name"];});
 export let VaccineGroupToAntigenMap = make(codesets.scheduleSupportingData.vaccineGroupToAntigenMap[0].vaccineGroupMap, (item) => {return item["name"];});
 export let CvxToAntigenMap = make(codesets.scheduleSupportingData.cvxToAntigenMap[0].cvxMap, (item) =>{ return "_" + item["cvx"];});
