ddcbd8cbaa38e687c250fb48ed44b3789ae191e6
Dennis Dunn <ansofive@gmail.com>
2016-08-21 13:50:39 -0400

    Update the gitignore to keep the generated .d.ts files.

---
+++ lib/LookupTableFactory.d.ts
@@ -0,0 +1,16 @@
+/**
+ * Given an array of objects and a property name, return an object
+ * with properties for each of the items where the property name is the
+ * value of the item.name property and the value is the item.
+ *
+ * Huh?
+ */
+export declare function New(data: any[], name: string): any;
+/**
+ * Return a a list made up of the properties of the item.
+ */
+export declare function ToList(item: any): any[];
+/**
+ * Group a list of items by the property name.
+ */
+export declare function GroupBy(data: any[], name: string): {};
---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+export declare let Antigens: any;
+export declare let LiveVirusConflicts: any;
+export declare let Contraindications: any;
+export declare let VaccineGroups: any;
+export declare let VaccineGroupToAntigenMap: any;
+export declare let CvxToAntigenMap: any;
